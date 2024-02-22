677c0a3c37d79157acbb87614d71d925e841fc93
jey3dayo <nakazato_junya@ca-adv.co.jp>
2024-02-23 02:41:19 +0900

    refactor: Simplify event handling in bot.py

--- bot.py
+++ bot.py
@@ -11,36 +11,41 @@ app = App(token=SLACK_BOT_TOKEN)
 # 反応する発言内容を記載
 @app.message("懇親会|飲み会|女子会|パーティ")
 def party_handler(body, say, client):
-    message = body["event"]
-    channel = message["channel"]
-    thread_ts = message["ts"]
+    event = body["event"]
 
-    response_message = openai_utils.get_party_call_response(client, message)
+    print("懇親会|飲み会|女子会|パーティが送られました")
+    response_message = openai_utils.get_party_call_response(client, event)
     if response_message.strip():
-        say(text=response_message, channel=channel, thread_ts=thread_ts)
+        say(text=response_message, channel=event["channel"], thread_ts=event["ts"])
 
 
 @app.message("^hi")
 def hi_handler(body, say):
-    message = body["event"]
-    channel = message["channel"]
-    thread_ts = message["ts"]
-
-    say(text="hi", channel=channel, thread_ts=thread_ts)
+    print("hiが送られました")
+    event = body["event"]
+    say(text=event["text"], channel=event["channel"], thread_ts=event["ts"])
 
 
 # mentionに反応
 @app.event("app_mention")
 def mention_handler(body, say):
-    message = body["event"]
-    text = message["text"]
-    channel = message["channel"]
-    thread_ts = message["ts"]
+    event = body["event"]
 
-    print(f"メンションされました: {text}")
-    response_message = openai_utils.get_chat_simple_response(text)
+    print(f"メンションされました: {event['text']}")
+    response_message = openai_utils.get_chat_simple_response(event["text"])
     if not response_message.strip() == "":
-        say(text=response_message, channel=channel, thread_ts=thread_ts)
+        say(text=response_message, channel=event["channel"], thread_ts=event["ts"])
+
+
+# DMに反応
+@app.event("message")
+def direct_message_handler(body, say):
+    event = body["event"]
+
+    print(f"DMが送られました: {event['text']}")
+    response_message = openai_utils.get_chat_simple_response(event["text"])
+    if not response_message.strip() == "":
+        say(text=response_message, channel=event["channel"], thread_ts=event["ts"])
 
 
 if __name__ == "__main__":
